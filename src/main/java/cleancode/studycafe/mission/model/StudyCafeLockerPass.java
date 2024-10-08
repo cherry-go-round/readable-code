f67578d6607582a2ecaf191dd81245fcacdd6cea
hongseokhodev <hongseokhodev@gmail.com>
2024-10-08 22:03:25 +0900

    if 문을 switch 문으로 변경

--- src/main/java/cleancode/studycafe/mission/model/StudyCafeLockerPass.java
+++ src/main/java/cleancode/studycafe/mission/model/StudyCafeLockerPass.java
@@ -29,16 +29,10 @@ public class StudyCafeLockerPass {
     }
 
     public String display() {
-        if (passType == StudyCafePassType.HOURLY) {
-            return String.format("%s시간권 - %d원", duration, price);
-        }
-        if (passType == StudyCafePassType.WEEKLY) {
-            return String.format("%s주권 - %d원", duration, price);
-        }
-        if (passType == StudyCafePassType.FIXED) {
-            return String.format("%s주권 - %d원", duration, price);
-        }
-        return "";
+        return switch (passType) {
+            case HOURLY -> String.format("%s시간권 - %d원", duration, price);
+            case WEEKLY, FIXED -> String.format("%s주권 - %d원", duration, price);
+        };
     }
 
 }
