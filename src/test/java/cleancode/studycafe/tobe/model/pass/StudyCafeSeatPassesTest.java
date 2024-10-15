8e401faeaa9aff6da848b3e933b6b79230fb6b30
hongseokhodev <hongseokhodev@gmail.com>
2024-10-15 14:22:54 +0900

    두 개의 테스트로 분리

--- src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassesTest.java
+++ src/test/java/cleancode/studycafe/tobe/model/pass/StudyCafeSeatPassesTest.java
@@ -9,24 +9,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class StudyCafeSeatPassesTest {
 
-    @DisplayName("타입에 알맞은 좌석 이용권만을 찾는다.")
+    @DisplayName("타입에 알맞은 좌석 이용권을 찾는다.")
     @Test
     void findPassByType() {
         //given
         StudyCafePassType findingType = StudyCafePassType.WEEKLY;
 
-        StudyCafeSeatPass findingPass1 = StudyCafeSeatPass.of(findingType, 1, 60000, 0.0);
-        StudyCafeSeatPass findingPass2 = StudyCafeSeatPass.of(findingType, 2, 100000, 0.1);
-        StudyCafeSeatPass unexpectedPass = StudyCafeSeatPass.of(StudyCafePassType.FIXED, 4, 250000, 0.1);
+        StudyCafeSeatPass expectedPass = StudyCafeSeatPass.of(findingType, 1, 60000, 0.0);
 
-        StudyCafeSeatPasses passes = StudyCafeSeatPasses.of(List.of(findingPass1, findingPass2, unexpectedPass));
+        StudyCafeSeatPasses passes = StudyCafeSeatPasses.of(List.of(expectedPass));
 
         //when
         List<StudyCafeSeatPass> result = passes.findPassBy(findingType);
 
         //then
-        assertThat(result).contains(findingPass1);
-        assertThat(result).contains(findingPass2);
+        assertThat(result).contains(expectedPass);
+    }
+
+    @DisplayName("타입이 다른 좌석 이용권은 찾지 않는다.")
+    @Test
+    void doesNotFindPassByDifferentType() {
+        //given
+        StudyCafePassType findingType = StudyCafePassType.WEEKLY;
+        StudyCafePassType differentType = StudyCafePassType.FIXED;
+
+        StudyCafeSeatPass unexpectedPass = StudyCafeSeatPass.of(differentType, 4, 250000, 0.1);
+
+        StudyCafeSeatPasses passes = StudyCafeSeatPasses.of(List.of(unexpectedPass));
+
+        //when
+        List<StudyCafeSeatPass> result = passes.findPassBy(findingType);
+
+        //then
         assertThat(result).doesNotContain(unexpectedPass);
     }
+
 }
